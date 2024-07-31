795b57779ac26ac05e7e54affd21504792b44f64
David ADE <david.ade.1989@gmail.com>
2024-07-31 20:18:44 +0200

    add evolutions

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const searchBtn = document.getElementById("search-button")
 const imgDiv = document.getElementById("img")
 const app = document.getElementById("pokemon-search-app")
 const pokeInfo = document.getElementById("pokemon-info")
+const pokeEvo = document.getElementById("pokemon-evolutions")
 // const pokeStats = document.getElementById("pokemon-stats")
 const background = document.getElementById("background");
 
@@ -53,6 +54,7 @@ const getPokemon = async name => {
     pkTypes.innerText = ""
     background.className = ""
     pokeInfo.classList.remove("hidden")
+    pokeEvo.innerHTML = ""
 
     // Fetch data
     const response = await fetch(pokeapi + name)
@@ -81,14 +83,62 @@ const getPokemon = async name => {
     pkSpeed.innerText = pokemon.stats[5]["base_stat"]
     progPkSpeed.value = pokemon.stats[5]["base_stat"]
   
+    // Display types
     for (let i = 0; i < pokemon.types.length; i++) {
       pkTypes.innerHTML += `
         <span class='type ${pokemon.types[i]["type"]["name"]}'>${pokemon.types[i]["type"]["name"].toUpperCase()}</span>
       `
     }
 
+    // Change background color
     background.classList.add(`bg-${pokemon.types[0]["type"]["name"]}`)
-  } catch {
+
+    // Evolution chain
+    let evolution = []
+    const evoChainUrl = pokemonSpecies.evolution_chain.url
+    const response3 = await fetch(evoChainUrl)
+    const evoData = await response3.json()
+    let evoChainData = evoData.chain
+    
+    do {
+      evolution.push({"name": evoChainData.species.name})
+      evoChainData = evoChainData['evolves_to'][0]
+    } while (!!evoChainData && evoChainData.hasOwnProperty('evolves_to'))  
+  
+
+    // Find evolution data
+    for (let i = 0; i<evolution.length; i++) {
+      const response4 = await fetch(pokeapi + evolution[i].name)
+      const evoData = await response4.json()
+      evolution[i].id = evoData.id
+      evolution[i].sprite = evoData.sprites["front_default"]
+      evolution[i].types = evoData.types
+    }
+
+    console.log(evolution)
+    // Display evolution chain
+    for (let i = 0; i < evolution.length; i++) {
+      pokeEvo.innerHTML += `
+        <div class="evolution">
+          <div>
+            <img src="${evolution[i].sprite}" alt="${evolution[i].name}" />
+          </div>
+          <div>
+            <p>${evolution[i].name.toUpperCase()} <span>${evolution[i].id}</span></p>
+            <p>
+              ${evolution[i].types.map(type => `
+                <span class='type ${type.type.name}'>${type.type.name.toUpperCase()}</span>
+              `).join("")}
+            </p>
+          </div>
+          
+          
+        </div>
+      `
+    }
+
+  } catch (error) {
+    console.error(error)
     alert('Pokémon not found')
   }
 }
